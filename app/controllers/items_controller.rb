eee2bacb4a7bc3175bd4096b16f354f9aa21d00e
k-takomal <foxinn009@gmail.com>
2021-01-25 20:02:36 +0900

    items_controllerの整頓
    不要なコメントアウトの削除
    updateにもユーザーにより遷移される記述を追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,18 +21,19 @@ def create
 end
 
 def show
-  # @item = Item.find(params[:id])
 end
 
 def edit
-  # @item = Item.find(params[:id])
   if current_user.id != @item.user_id
     redirect_to root_path
   end
 end
 
 def update
-  # @item = Item.find(params[:id])
+  if current_user.id != @item.user_id
+    redirect_to root_path
+  end
+
   if @item.update(item_params)
     redirect_to item_path(@item)
   else
